2404256ec64523bd2c9d3a4f72bac1b3efa6ab83
lokeshkavuluru <lokamanyaaaa@gmail.com>
2016-09-21 23:18:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 //edit the text using js
 var element = document.getElementById('main-text');
 
-element.InnerHTML = 'New experience using java script has beeen served';
+element.innerHTML = 'New experience using java script has beeen served';
